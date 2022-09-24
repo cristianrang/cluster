6a451de5007ff4416721cd090048e8f112f214d1
Cristian Eluney Rangel Campos <celuneyrc@hotmail.com>
2022-09-24 13:42:55 -0500

    Se modifica grupo de seguridad, agrega un nodo y se instala el docker swarm

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ provider "aws" {                                          #Provider es el provee
 
 resource "aws_instance" "Docker-Swarm" {
   instance_type          = "t2.micro"
-  count = 3
+  count = 4
   ami                    = "ami-08d4ac5b634553e16"
   tags = {
     "Name" = "Node-${count.index}"
@@ -38,6 +38,20 @@ resource "aws_security_group" "DockerWebSG" { #Vamos a crear un grupo de segurid
     from_port   = 22                     #Del puerto
     to_port     = 22                     #Al puerto
     protocol    = "tcp"                  #Protocolo
+  }
+  ingress {
+    cidr_blocks = ["172.31.85.220/32","172.31.88.34/32", "172.31.84.166/32", "172.31.82.132/32"] #Solo puede entrar esta IP
+    description = "SG Docker Swarm"               #Descripción
+    from_port   = 2377                     #Del puerto
+    to_port     = 2377                     #Al puerto
+    protocol    = "tcp"                  #Protocolo
+  }
+  ingress {                     #Reglas de firewall de entrada
+    cidr_blocks = ["0.0.0.0/0"] #Se aplicará a todas las direcciones
+    description = "SG HTTP Visualizer"     #Descripción
+    from_port   = 8080            #Del puerto
+    to_port     = 8080            #Al puerto
+    protocol    = "tcp"         #Protocolo
   }
   egress {                                  #Reglas de firewall de salida
     cidr_blocks = ["0.0.0.0/0"]             #Se aplicará a todas las direcciones
